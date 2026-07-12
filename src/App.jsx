bc7d57b4301322ff1a8b398ca926441c9bcb5d3f
dthakor1555-png <dthakor1555@gmail.com>
2026-07-12 19:32:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,11 @@ const API_KEY = '0627e9682f6c3eca80da4e2a6217ce57';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
 const POSTER_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
 // Embed Links
+const getMovieEmbedUrl = (tmdbId) => `https://vidsrc.sbs/embed/movie/${tmdbId}`;
+const getTvEmbedUrl = (tmdbId, season, episode) => `https://vidsrc.sbs/embed/tv/${tmdbId}/${season}/${episode}`;
 
-
-const getMovieEmbedUrl = (tmdbId) => 
-  `https://vidsrc.sbs/embed/movie/${tmdbId}`;
-const getTvEmbedUrl = (tmdbId, season, episode) => 
-  `https://vidsrc.sbs/embed/tv/${tmdbId}/${season}/${episode}`;
 // --- UTILS ---
 const fetchApi = async (endpoint) => {
   try {
